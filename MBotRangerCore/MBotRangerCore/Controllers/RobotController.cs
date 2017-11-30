cc9d42bc4a6a09307a6ced8ceb3f85970a9edbce
karamfikre <karam_love.aldolaimy@yahoo.com>
2017-11-30 15:13:36 +0100

    Mouse option is Added

--- MBotRangerCore/MBotRangerCore/Controllers/RobotController.cs
+++ MBotRangerCore/MBotRangerCore/Controllers/RobotController.cs
@@ -61,5 +61,38 @@ namespace MBotRangerCore.Controllers
 
             return View();
         }
+        public IActionResult Mouse(string submit)
+        {
+
+            switch (submit)
+            {
+                case "Forward":
+                    sendbuf = Encoding.ASCII.GetBytes("1");
+                    break;
+                case "Back":
+                    sendbuf = Encoding.ASCII.GetBytes("2");
+                    break;
+                case "Go Left":
+                    sendbuf = Encoding.ASCII.GetBytes("3");
+                    break;
+                case "Go Right":
+                    sendbuf = Encoding.ASCII.GetBytes("4");
+                    break;
+                case "Stop":
+                    sendbuf = Encoding.ASCII.GetBytes("5");
+                    break;
+                default:
+                    sendbuf = Encoding.ASCII.GetBytes("0");
+                    break;
+            }
+
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+              ProtocolType.Udp);
+
+            IPAddress broadcast = IPAddress.Parse("192.168.4.1");
+            IPEndPoint ep = new IPEndPoint(broadcast, 1025);
+            s.SendTo(sendbuf, ep);
+            return View();
+        }
     }
 }
