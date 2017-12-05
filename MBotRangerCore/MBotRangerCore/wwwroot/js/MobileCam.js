46a3abcd60b5b4787c9c42b035d59f83270ccaf3
Ahed Ali <ahedali@gmail.com>
2017-12-05 15:29:12 +0100

    Progress to do the capture function

---
+++ MBotRangerCore/MBotRangerCore/wwwroot/js/MobileCam.js
@@ -0,0 +1,20 @@
+﻿var img = document.getElementById("captureImage");
+
+var button = document.querySelector("button");
+
+var containerImg = document.getElementById("captureImage");
+
+var canvas = document.createElement("canvas");
+
+canvas.width = 500;
+canvas.height = 330;
+
+var ctx = canvas.getContext("2d");
+
+
+button.addEventListener("click", function () {
+    ctx.drawImage(containerImg, 100, 100)
+    containerImg.src = canvas.toDataURL();
+});
+
+
