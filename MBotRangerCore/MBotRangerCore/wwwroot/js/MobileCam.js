711270be7fd76105608970f84b310959cdb451e9
Ahed Ali <ahedali@gmail.com>
2017-12-06 14:38:59 +0100

    Adding stop and play buttons on Mobile Camera.

--- MBotRangerCore/MBotRangerCore/wwwroot/js/MobileCam.js
+++ MBotRangerCore/MBotRangerCore/wwwroot/js/MobileCam.js
@@ -1,20 +1,27 @@
-﻿var img = document.getElementById("captureImage");
-
-var button = document.querySelector("button");
-
+﻿
+//Display container
+var img = document.getElementById("mobileVideo");
 var containerImg = document.getElementById("captureImage");
-
-var canvas = document.createElement("canvas");
-
-canvas.width = 500;
-canvas.height = 330;
-
+var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext("2d");
 
+//Buttons
+var stopStreaming = document.getElementById("stopStreaming");
+var capture = document.getElementById("capture");
+var play = document.getElementById("playVideo");
 
-button.addEventListener("click", function () {
-    ctx.drawImage(containerImg, 100, 100)
-    containerImg.src = canvas.toDataURL();
+//Capture the image through streaming
+capture.addEventListener("click", function () {
+    ctx.drawImage(img, 0, 0 ,500,330)
+    canvas.src = canvas.toDataURL('image/jpeg', 1.0);
 });
 
+//Stop streaming of the mobile camera
+stopStreaming.addEventListener("click", function () {
+    img.src = "//:0";
+});
 
+//Play video of the mobile camera
+play.addEventListener("click", function () {
+    img.src = "http://192.168.1.130:8080/video";
+});
